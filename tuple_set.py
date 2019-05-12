a2c546f65b0336df668d61506b497511baf16e02
Md. Masudur Rahman <masudurhimel@gmail.com>
2019-05-12 23:47:56 +0600

    tuple_set

---
+++ tuple_set.py
@@ -0,0 +1,23 @@
+# tuple
+
+x = (1, 2, 3)
+print(x)
+
+# set
+# set doesn't take repeated values
+#set has no order , it follows a unique algo to order things
+
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(3)
+x.add(4)
+x.add(4)
+
+print(x)
+
+x = set([1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4])
+x.add(5)
+x.add(6)
+print(x)
