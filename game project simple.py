c599bdcbf28b69f56328faee86bbc87b66d2759f
Md. Masudur Rahman <masudurhimel@gmail.com>
2019-05-13 16:49:33 +0600

    simple game added

---
+++ game project simple.py
@@ -0,0 +1,47 @@
+import random
+
+
+def random_generator():
+    x = list(range(10))
+    r = ""
+    random.shuffle(x)
+    for i in x:
+        r += str(i)
+
+    return r[:3]
+
+
+def generate_clues(ai_guess, user_guess):
+    clue = []
+
+    for i in range(3):
+        if ai_guess[i] == user_guess[i]:
+            clue.append("Match")
+        elif user_guess[i] in ai_guess:
+            clue.append("Close")
+
+    if clue == []:
+        clue.append("Nope")
+    return clue
+
+
+
+ai_guess = random_generator()
+print("Computer Value is generated\n")
+print(ai_guess)
+
+user_guess = input("Guess a number of 3 digit : ")
+
+i = ""
+while i != "Code Cracked!!":
+    if ai_guess == user_guess:
+        i = "Code Cracked!!"
+        for j in i:
+            print(j)
+        break
+
+    print("Clues : ")
+    clues = generate_clues(ai_guess, user_guess)
+    for i in clues:
+        print(i)
+    user_guess = input("Guess a number of 3 digit : ")
--- temp.py
+++ temp.py
@@ -1,4 +1,22 @@
-x = "himel"
+def generate_clues(ai_guess, user_guess):
+    clue = []
 
-for i in x:
+    for i in range(3):
+        if ai_guess[i] == user_guess[i]:
+            clue.append("Match")
+        elif user_guess[i] in ai_guess:
+            clue.append("Close")
+
+    if clue == []:
+        clue.append("Nope")
+    return clue
+
+
+ai_guess = "123"
+
+user_guess = "456"
+
+clues = generate_clues(ai_guess, user_guess)
+
+for i in clues:
     print(i)
